04af9e76809e8bf4e1ad2f1b8e0c6624ad90b2ad
shadowpog <79033248+shadowpog@users.noreply.github.com>
2021-05-17 12:49:53 +0600

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 891852901 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/KPRAAHUE/CarHub/main/greenvilleobfuscated.lua'))()
+elseif game.PlaceId == 54865335 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/KPRAAHUE/CarHub/main/ultimatedrivingobfuscated.lua'))()
+elseif game.PlaceId == 3351674303 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/KPRAAHUE/CarHub/main/drivingempireobfuscated.lua'))()
+end
