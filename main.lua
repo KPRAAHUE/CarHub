4c00bc5b97f83b9e4e8f501897a0c3aec215fb09
shadowpog <79033248+shadowpog@users.noreply.github.com>
2021-05-20 17:00:48 +0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,4 +6,6 @@ elseif game.PlaceId == 3351674303 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/KPRAAHUE/CarHub/main/drivingempireobfuscated.lua'))()
 elseif game.PlaceId == 4923142893 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/KPRAAHUE/CarHub/main/acceleratex.lua'))()
+elseif game.PlaceId == 4566572536 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/KPRAAHUE/CarHub/main/vehiclelegends.lua'))()
 end
