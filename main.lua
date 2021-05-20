3f3dbc53d7280293b8a06c608d38752a4d387246
shadowpog <79033248+shadowpog@users.noreply.github.com>
2021-05-20 18:02:10 +0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 if game.PlaceId == 891852901 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/KPRAAHUE/CarHub/main/greenvilleobfuscated.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/KPRAAHUE/CarHub/main/greenville.lua'))()
 elseif game.PlaceId == 171391948 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/KPRAAHUE/CarHub/main/vehiclesimobfuscated.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/KPRAAHUE/CarHub/main/vehiclesim.lua'))()
 elseif game.PlaceId == 3351674303 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/KPRAAHUE/CarHub/main/drivingempireobfuscated.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/KPRAAHUE/CarHub/main/drivingempire.lua'))()
 elseif game.PlaceId == 4923142893 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/KPRAAHUE/CarHub/main/acceleratex.lua'))()
 elseif game.PlaceId == 4566572536 then
