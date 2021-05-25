0fff3b2655d0ca10a3aeb461807da1a6f73e4149
shadowpog <79033248+shadowpog@users.noreply.github.com>
2021-05-25 15:44:15 +0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,4 +8,6 @@ elseif game.PlaceId == 4923142893 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/KPRAAHUE/CarHub/main/acceleratex.lua'))()
 elseif game.PlaceId == 4566572536 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/KPRAAHUE/CarHub/main/vehiclelegends.lua'))()
+elseif game.PlaceId == 2418863943 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KPRAAHUE/CarHub/main/roanoke.lua"))()
 end
